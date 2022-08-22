359d2ef5155cf7434c4f38d9749a37e78a63fdf1
linhdo-dql <linhlee88999@gmail.com>
2022-08-22 19:15:41 +0700

    Tiến độ 22/8 (Hoàn thiện trang chủ)

--- HomePortal/wwwroot/js/app.js
+++ HomePortal/wwwroot/js/app.js
@@ -1,5 +1,8 @@
 ﻿window.onload = function () {
     console.log("chạy");
+   
+
+
     
 }
 window.initializeCarousel = () => {
@@ -33,5 +36,113 @@ window.initializeCarousel = () => {
             });
         }
 
+        countUp('.counter0');
+        countUp('.counter1');
+        countUp('.counter2');
+        countUp('.counter3');
+
+        
+       
+
     })
+
+    // Header Sticky
+    window.addEventListener("scroll", function () {
+        let navbarArea = this.document.getElementById('navbar-area');
+        if (window.pageYOffset > 0) {
+            navbarArea.classList.add('nav-on-scroll');
+        }
+        else {
+            navbarArea.classList.remove('nav-on-scroll');
+        }
+    })
+
+    
+}
+
+function showDropdownMenu(obj) {
+    var dropDownMenu = obj.parentNode.parentNode.lastChild;
+    
+    if (dropDownMenu.style.display == 'block') {
+        dropDownMenu.style.display = 'none';
+        obj.classList.remove('fa-minus');
+        obj.classList.add('fa-plus');
+    }
+    else {
+        dropDownMenu.style.display = 'block';
+        obj.classList.add('fa-minus');
+        obj.classList.remove('fa-plus');
+    }
+    const pmMenus = document.querySelectorAll('.p-m-menu');
+
+    pmMenus.forEach(pmm => {
+        if (obj === pmm) {
+            
+        }
+        else {
+            var dropMenu = pmm.parentNode.parentNode.lastChild;
+            dropMenu.style.display = 'none';
+            pmm.classList.remove('fa-minus');
+            pmm.classList.add('fa-plus');
+        }
+    });
+
+
+    // Button Hover JS
+    $(function () {
+        $('.default-btn')
+            .on('mouseenter', function (e) {
+                var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({ top: relY, left: relX })
+            })
+            .on('mouseout', function (e) {
+                var parentOffset = $(this).offset(),
+                    relX = e.pageX - parentOffset.left,
+                    relY = e.pageY - parentOffset.top;
+                $(this).find('span').css({ top: relY, left: relX })
+            });
+    });
+
+// originally forked from https://codepen.io/kkick/pen/oWZMov
+
+}
+
+
+
+function countUp(cl) {
+    const counterUp = window.counterUp.default
+
+    const callback = entries => {
+        entries.forEach(entry => {
+            const el = entry.target
+            if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                counterUp(el, {
+                    duration: 2000,
+                    delay: 16,
+                })
+                el.classList.add('is-visible')
+            }
+        })
+    }
+
+    const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+    const el = document.querySelector(cl)
+    IO.observe(el)
+
+    $('.dropdown').on('show.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+    $('.dropdown').on('hide.bs.dropdown', function (e) {
+        e.preventDefault();
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(400, function () {
+            $(this).closest('.dropdown').removeClass('open');
+            $(this).closest('.dropdown').find('.dropdown-toggle').attr('aria-expanded', 'false');
+        });
+    });
+
+
+
 }
