9323597fb99a5cba95bc1728069c168498702df7
linhdo-dql <linhlee88999@gmail.com>
2022-08-30 17:56:56 +0700

    Tiến độ 30/8 (thêm include html, sửa lỗi lần 2)

--- BByteHomePortal/js/app.js
+++ BByteHomePortal/js/app.js
@@ -1,5 +1,6 @@
 ﻿window.onload = function () {
     console.log("chạy");
+    includeHTML();
     new WOW().init();
     // Spinner
     var spinner = function () {
@@ -229,4 +230,34 @@ function closePopupImage() {
     var popupImage = document.getElementById("product-img-popup");
     popupImage.style.display = 'none';
     popup.style.visibility = 'hidden';
+}
+
+
+//Include HTML
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
 }
